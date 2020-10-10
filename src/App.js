9fd8205d2c847df195be4b9b66eb948f13ca426b
karmaggyatso <karmaggyatso@gmail.com>
2020-10-10 12:30:53 -0400

    new Change

--- src/App.js
+++ src/App.js
@@ -1,133 +1,61 @@
-import React, { Component, useCallback } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
 function City(props) {
-  const {city, state, latitude, longitude, population, totalWages, isEmpty} = props;
   return (
-      <div>
-        {isEmpty? 
-          <div className = "container align-center card text-left mt-2" style = {{width: "26rem"}}>
-          <h4 className = "card-title p-3">{city}, {state}</h4>
-          <ul className = "">
-            <li>State: {state}</li>
-            <li>Location: ({latitude}, {longitude})</li>
-            <li>Population (estimated): {population}</li>
-            <li>Total Wages: {totalWages}</li>
-          </ul>
-        </div>
-        : <div>
-            <p>{props.message}</p>
-          </div>
-      }
-      
+    <div className="card">
+      <h3>{props.data.City}, {props.data.State}</h3>
+      <ul>
+        <li>Population: {props.data.EstimatedPopulation}</li>
+        <li>Location: ({props.data.Lat}, {props.data.Long})</li>
+      </ul>
     </div>
-    );
-    
+  );
 }
-
 function ZipSearchField(props) {
-  const {zipChange, userInputZip} = props;
-  
-  return (<div>
-     <form>
-            <label htmlFor = "search-input">Zip Code:</label>
-            <input type = "text" 
-              value = {userInputZip}
-              placeholder = "Try 10016"
-              onChange = {zipChange}
-              />
-          </form>
-  </div>);
+  return (
+    <div>
+      Type in a zip code: <input type="text" onChange={(e) => props.zipChanged(e)} value={props.value} />
+      <p>The zip code entered is: { props.value}</p>
+    </div>);
 }
- function checksize(arr) {
-  return arr.length;
-}
-
 class App extends Component {
   state = {
-    item: [],
-    message: 'No Results',
-    zipCode: '',
-    result: false,
+    inputZip: "",
+    cityResults: [],
   }
-
-
-  handleOnChange = (event) => {
-    const query = event.target.value;
-    const xhr = new XMLHttpRequest();
-    
+  handleZipChange(e) {
     this.setState({
-      zipCode: event.target.value,
-    });
-    if (checksize(query) !== 5 || checksize(query) > 5 || checksize(query) < 5) {
-      return (
-        <div>
-          <p>{this.state.message}</p>
-        </div>
-      )
-    } 
-    else if (xhr.status) {
-      console.log(xhr.state)
-      return (
-        <div>
-          <City isEmpty = {this.state.status} />
-        </div>
-      )
-    }
-    else {
-      fetch(`http://ctp-zip-api.herokuapp.com/zip/${query}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          item: data,
-          status: true
+      inputZip: e.target.value,
+    })
+    if(e.target.value.length === 5) {
+      fetch("https://ctp-zip-api.herokuapp.com/zip/" + e.target.value)
+        .then(res => res.json())
+        .then(jsonData => {
+          this.setState({
+            cityResults: jsonData,
+          })
         })
-      })
-      .catch (error =>{
-        console.error(error);
-      });
-      
+        .catch(err => {
+          this.setState({ cityResults: [] })
+        })
+    } else {
+      this.setState({ cityResults: [] })
     }
-      
   }
-
   render() {
-    const {zipCode, message} = this.state; // destructuring .. meaning we dont have to type this.state.zipCode everytime
-    //console.log(zipCode, this.state.pupulation);
-
-    const {items} = this.state;
-
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <div className = "container text-center m-3 ">
-         
-
-          <ZipSearchField zipChange = {(e) => this.handleOnChange(e)}/>
-
-          <div>
-            {
-              this.state.item.map(city => (
-                <City
-                  key={city.RecordNumber} 
-                  city = {city.City}
-                  state = {city.State}
-                  latitude= {city.Lat}
-                  longitude = {city.Long}
-                  population = {city.EstimatedPopulation}
-                  totalWages = {city.TotalWages}
-                  isEmpty = {this.state.status}
-                />
-              ))
-            }
-          </div>
+        <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} value={this.state.inputZip} />
+        <div>
+          { this.state.cityResults.map((item, index) => {
+            return <City data={item} key={index} />;
+          }) }
         </div>
       </div>
     );
   }
 }
-
 export default App;
